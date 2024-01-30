d6ec4a77cb73be8ff70eaebc1c004a04568fb083
김태관 <>
2024-01-30 14:55:38 +0900

    no message

---
+++ Running Game/Assets/DinoRun_Scripts/Dino.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : MonoBehaviour
+{
+    public float startJumpPower;
+    public float jumpPower;
+    public bool isGround;
+
+    private Rigidbody2D rigid;
+    private Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
